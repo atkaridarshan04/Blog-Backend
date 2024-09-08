c3c2d6da477f862e167b685ac0a6b2d046c8a450
atkaridarshan04 <atkaridarshan04@gmail.com>
2024-09-08 20:45:07 +0530

    Add middleware for CORS, JSON parsing, URL encoding, static files, and cookie parsing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connect.js'; 
 import initDB from './db/init.js';
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 // Load environment variables
 dotenv.config();
 
 const app = express();
+
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 5000;
 
 // Establish MySQL connection
