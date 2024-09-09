f7d9a7e4b9d69fb35dfe0af49704b81fd1337173
atkaridarshan04 <atkaridarshan04@gmail.com>
2024-09-09 22:31:24 +0530

    add update post endpoint

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import e from "express";
 import Post from "../models/posts.model.js";
 
 export const createPost = async (req, res) => {
@@ -18,5 +19,31 @@ export const createPost = async (req, res) => {
         res.status(201).json({ message: "Post created successfully!", post });
     } catch (error) {
         res.status(500).json({ message: 'Server error', error: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const ownerId = req.user.id;
+
+        const post = await Post.findByPk(postId)
+        if (!post) {
+            res.status(404).json({ message: "Post not found" })
+        }
+
+        if (!ownerId === post.ownerId) {
+            res.status(401).json({ message: "Unauthorized request " })
+        }
+
+        const { title, content } = req.body
+        if (title) post.title = title
+        if (content) post.content = content
+
+        await post.save();
+
+        return res.status(200).json({ message: "Post updated successfully", post })
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message })
     }
 }
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { verifyJwt } from "../middleware/auth.middleware.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, updatePost } from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.post('/create', verifyJwt, createPost)
+
+router.put('/update/:id', verifyJwt, updatePost)
 
 export default router;
